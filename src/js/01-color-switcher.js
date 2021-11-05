9ddecec847c20e7c2a0ef0973b2dc23acb888ef8
LutsenkoVitaliy <lutsenko996@gmail.com>
2021-11-05 16:01:04 +0200

    randomColorEnd

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,20 @@
+const ref = {
+    startBtn : document.querySelector('[data-action="start"]'),
+    stopBtn: document.querySelector('[data-action="stop"]'),
+}
+const PROMPT_DELAY = 1000;
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+ref.startBtn.addEventListener('click', () => {
+    onClickStart = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+        ref.startBtn.setAttribute("disabled", true)
+    }, PROMPT_DELAY)
+}),
+ref.stopBtn.addEventListener('click', () => {
+    clearInterval(onClickStart),
+        ref.startBtn.removeAttribute("disabled")
+})
 
